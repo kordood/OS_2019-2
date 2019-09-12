c8bd86e520518ca5c0405a6fb2241f3f7d3a10ab
kordood <gigacms@gmail.com>
2019-09-12 16:15:23 -0700

    temporary upload

--- 00.BootLoader/BootLoader.asm
+++ 00.BootLoader/BootLoader.asm
@@ -61,51 +61,64 @@ START:
 .MESSAGEEND:
     jmp $
 
+.BCDTOHEX:						; arg: ax, result: bx
+	push cx
+	mov cl, al
+
+	shr al, 4					; 10's place
+	mov bh, al
+	
+	mov al, cl
+	and al, 0x0F				; 1's place
+	mov bl, al
+
+	pop cx
+	ret
+
+.BCDTOASCII:
+	call .BCDTOHEX
+	add bh, 48
+	add bl, 48
+	ret
+
+.HEXCONVERT:
+	call .BCDTOHEX
+	push cx
+	mov al, bh
+	mov cl, 10
+	mul cl
+	add bl, al
+	pop cx
+	
+
 .GETRTC:
 	pusha
 
 	mov ah, 4h					; Select 'Read RTC Calendar'
 	int 1Ah						; RTC sevices interrupt
 
-	mov al, ch					; Get century
-	shr al, 4					; Take 10's place
-	add al, 48					; bit to ascii
-	mov [CLOCK_STRING+20], al	; Saving to string
+	mov word[CCYY], cx
+	mov word[MMDD], dx
 
 	mov al, ch
-	and al, 0x0F				; Take 1's place
-	add al, 48
-	mov [CLOCK_STRING+21], al
+	call .BCDTOASCII
+	mov [CLOCK_STRING+20], bh	; Saving to string
+	mov [CLOCK_STRING+21], bl
 
 	mov al, cl					; Get year
-	shr al, 4
-	add al, 48
-	mov [CLOCK_STRING+22], al
-
-	mov al, cl
-	and al, 0x0F
-	add al, 48
-	mov [CLOCK_STRING+23], al
+	call .BCDTOASCII
+	mov [CLOCK_STRING+22], bh
+	mov [CLOCK_STRING+23], bl
 
 	mov al, dh					; Get month
-	shr al, 4
-	add al, 48
-	mov [CLOCK_STRING+17], al
-
-	mov al, dh
-	and al, 0x0F
-	add al, 48
-	mov [CLOCK_STRING+18], al
+	call .BCDTOASCII
+	mov [CLOCK_STRING+17], bh
+	mov [CLOCK_STRING+18], bl
 
 	mov al, dl					; Get date
-	shr al, 4
-	add al, 48
-	mov [CLOCK_STRING+14], al
-
-	mov al, dl
-	and al, 0x0F
-	add al, 48
-	mov [CLOCK_STRING+15], al
+	call .BCDTOASCII
+	mov [CLOCK_STRING+14], bh
+	mov [CLOCK_STRING+15], bl
 
 	call .GETYOIL
 
@@ -113,118 +126,167 @@ START:
 	ret
 
 .GETYOIL:
-	mov ax, cx
-	mov bx, dx
-	push cx
-	push dx
-
-	mov cx, ax					; cx <--- [ YY | YY ]
+	mov cx, word[CCYY]			; cx <--- [ YY | YY ]
 	call .CALCYYYY
 	mov ax, cx
 
-	mov cx, bx					; cx <--- [ MM | DD ]
-	call .CALCMMDD
+	mov dx, word[MMDD]			; dx <--- [ MM | DD ]
+	push ax
+	;call .CALCMM
+	pop ax
+	
+	;add ax, result		mov bx, 7		div bx		dx = index
 
 	pop dx
 	pop cx
 	ret
 
 .CALCYYYY:
-	push bx						; save MMDD
-	mov dh, 19
-	mov dl, 00					; dx <--- [ 19 | 00 ]
-
-	mov al, ch
-	and al, 0x0F				; 1's place
-	mov bl, al
-
 	mov al, ch					; century
-	shr al, 4
-
-	mov bh, 10					; 10's place
-	mul bh
-
-	add bl, al
-	sub bl, dh					; century(year) - 19(00)
+	call .HEXCONVERT
+	
+	sub bl, 19					; century(year) - 19(00)
 
 	mov ax, bx					; result 100's place
 	mov bx, 100
 	mul bx
-	mov bx, ax
+	mov bx, ax					; 20()() - 19()() = 1()() => bx
 	
 	mov al, cl
-	and al, 0x0F				; year
-	add bl, al
+	mov cx, bx					; result move to cx
+	call .HEXCONVERT
 
-	mov al, cl
-	shr al, 4
-	mov ch, 10
-	mul ch
-	add bl, al
-	sub bl, dl					; bx contains result of yyyy - 1900		ex) [ 119 ]
+	sub cx, 00					; (century)year - (19)00
 
-	mov cx, bx					; result
+	mov word[YEARGAP], cx
 
-	call .YOIL1
+	call .YEARYOON
 
-	pop bx
 	ret
 
-.YOIL1:
+.YEARYOON:
 	mov ax, cx
-	mov dx, 4
-	div dx						; ex) 119 / 4 = ax ... dx
-	mov bx, ax
+	mov bx, 4
+	div bx						; ex) 119 / 4 = ax ... dx
+	mov cx, ax					; ex) cx = 29
 
-	mov ax, cx
-	mov dx, 100
-	div dx
-	sub bx, ax					; ex) 29 - floor(119 / 100) = 28(bx)
+	mov ax, word[YEARGAP]
+	mov bx, 100
+	div bx
+	sub cx, ax					; ex) 29 - floor(119 / 100) = 28(bx)
 
-	mov ax, cx
-	mov dx, 400
-	div dx
+	mov ax, word[YEARGAP]
+	mov bx, 400
+	div bx
 	mov ax, dx
-	mov dx, 100
-	div dx
-	add bx, ax					; ex) 28(bx) + floor(119 mod(400)) / 100) = 29(bx)
-
-	sub cx, bx					; ex) cx: 119 - 29, bx: 29
-
-	mov ax, cx
-	mov dx, 7
-	div dx
+	mov dx, 0					; Sanitize dx
+	mov bx, 100
+	div bx						; ex) floor(119 mod(400) / 100)
+	cmp ax, 1
+	jne .YOIL1_PASS1
+	
+	push ax						; ex) ax = floor(119 mod(400)) / 100)
 	mov ax, dx
-	mov dl, 1
-	mul dl 						; ex) 90 mod(7) * 365 mod(7)
-	mov cx, ax
+	mov dx, 0					; Sanitize dx
+	mov bx, 4					; ex) 19 / 4
+	div bx
+	pop ax
 
-	mov ax, bx
-	mov dx, 7
-	div dx
+	cmp dx, 0
+	jne .YOIL1_PASS1
+	mov byte[YOONFLAG], 1		; save isYoon 1
+
+.YOIL1_PASS1:
+	add cx, ax					; ex) 28(bx) + floor(119 mod(400)) / 100) = 29(bx)
+
+	sub word[YEARGAP], bx		; ex) cx: 119 - 29, bx: 29
+	push bx
+
+	mov ax, word[YEARGAP]
+	mov dx, 0					; Sanitize dx
+	mov bx, 7
+	div bx						; ex) 90 mod(7) * 365 mod(7) = 6 * 1(const)
+	mov cx, dx					; cx = result
+
+	pop ax						; pop bx + mov ax, bx
+
+	mov bx, 7
+	div bx
 	mov ax, dx
-	mov dl, 2
-	mul dl						; ex) 29 mod(7) * 366 mod(7)
+	mov bx, 2
+	mul bx						; ex) 29 mod(7) * 366 mod(7) = 1 * 2(const)
 
 	add ax, cx
-	mov dx, 7
-	div dx
-	mov cx, dx					; cx mod(7)
+	mov bx, 7
+	div bx
+	mov cx, dx					; ex) cx = (90 mod(7) * 365 mod(7) +  29 mod(7) * 366 mod(7)) mod(7)
 
-
-
-
-.CALCMMDD:
-	push ax
-	mov dh, 01
-	mov dl, 01					; dx <--- [ 01 | 01 ]
-
-	pop ax
 	ret
 
+.CALCMM:
+	mov al, dh					; month(bcd)
+	call .HEXCONVERT
+	mov cx, 0
 
-MESSAGE1:    db 'MINT64 OS Boot Loader Start~!!', 0
-CLOCK_STRING: db 'Current Data: 00/00/0000 FFF', 0
+.MONTHLOOP:
+	sub bl, 1					; month--
+	cmp bl, 0
+	je .CALCDD					; if(month == 0) jmp .CALCDD
+	
+	add cl, 1					; index++
+	mov al, byte[IDXMONTH + 1]
+
+	cmp al, 1
+	je .MONTH31
+	
+	cmp al, 2
+	je .MONTH30
+
+	cmp byte[YOONFLAG], 1					; isYoon compare to 1
+	je .IFYOON
+	
+	add word[DATE], 28
+	jmp .MONTHLOOP
+
+.IFYOON:
+	add word[DATE], 29
+	jmp .MONTHLOOP
+
+.MONTH31:
+	add word[DATE], 31
+	jmp .MONTHLOOP
+
+.MONTH30:
+	add word[DATE], 30
+	jmp .MONTHLOOP
+
+.CALCDD:
+	mov al, dl					; date(bcd)
+	call .HEXCONVERT
+
+	mov ax, word[DATE]			; month(each date) + date
+	add al, bl
+	mov cx, 7
+	div cx						; dx is index of yoil
+
+	mov ax, dx
+	mov dx, 0
+	mov cx, 3
+	mul cx
+
+	ret
+	
+
+MESSAGE1:		db 'MINT64 OS Boot Loader Start~!!', 0
+CLOCK_STRING:	db 'Current Data: 00/00/0000 FFF', 0
+YOIL:			db 'MONTUEWEDTHUFRISATSUN', 0
+
+CCYY: dw 0
+MMDD: dw 0
+IDXMONTH:	db '131212112121', 0
+DATE:			dw 0
+YEARGAP:		dw 0
+YOONFLAG:		db 0
 
 times 510 - ( $ - $$ )    db    0x00
 
